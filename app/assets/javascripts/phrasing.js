e598c72cab69e2f59917b300e6d5100f3d871e5b
Kalach Vladislav <kalach.vladislav@gmail.com>
2018-07-25 21:53:10 +0300

    output the value when there is no translation

--- app/assets/javascripts/phrasing.js
+++ app/assets/javascripts/phrasing.js
@@ -113,7 +113,7 @@ var phrasing_setup = function(){
     var content = record.innerHTML;
 
     if(content.length === 0){
-      content = " ";
+      content = "Нет перевода";
     }
 
     $.ajax({
@@ -122,7 +122,7 @@ var phrasing_setup = function(){
       data: { new_value: content },
       success: function(e){
         userTriggeredPhrasingDOMChange = false;
-        if(content === " "){
+        if(content === "Нет перевода"){
           $('span.phrasable[data-url="'+ url +'"]').html(content);
         }else{
           // Not to lose the cursor on the current contenteditable element
